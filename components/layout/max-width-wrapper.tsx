420f3f1590cab25dcabbadc9160fd9d3228caa4d
bagoesrex <bagoesrex@gmail.com>
2025-10-29 19:49:41 +0700

    feat(layout): add max-width-wrapper component

---
+++ components/layout/max-width-wrapper.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+interface MaxWidthWrapperProps {
+    children: ReactNode
+    className?: string
+}
+
+export default function MaxWidthWrapper({ children, className }: MaxWidthWrapperProps) {
+    return (
+        <div
+            className={cn(
+                "mx-auto w-full max-w-7xl px-2",
+                className
+            )}
+        >
+            {children}
+        </div>
+    );
+}
