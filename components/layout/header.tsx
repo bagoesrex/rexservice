6fffad558e031ae9319a86ce021f2322c29aad9c
bagoesrex <bagoesrex@gmail.com>
2025-10-29 19:22:55 +0700

    style(header): adjust responsive padding

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="w-full bg-white/80 backdrop-blur-md px-2">
+        <header className="w-full bg-white/80 backdrop-blur-md px-0 lg:px-2">
             <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2.5 shadow-[0px_2px_5px_rgba(0,0,0,0.27)] rounded-xs">
                 <Link href={"/"} className="flex items-center gap-3 text-xl font-semibold text-gray-800">
                     <Image src={"/logo.svg"} alt={"main icon"} width={32} height={32} priority />
