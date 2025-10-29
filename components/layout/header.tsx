cca820e467a0862e240c869d7b56ce079b4b02cd
bagoesrex <bagoesrex@gmail.com>
2025-10-29 20:01:17 +0700

    style(header): add hover underline and refine layout

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="w-full bg-white/80 backdrop-blur-md px-0 lg:px-2">
+        <header className="w-full bg-white/80 backdrop-blur-md px-0 lg:px-2 fixed">
             <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2.5 shadow-[0px_2px_5px_rgba(0,0,0,0.27)] rounded-xs">
-                <Link href={"/"} className="flex items-center gap-3 text-xl font-semibold text-gray-800">
+                <Link href={"/"} className="flex items-center gap-3 text-xl font-semibold text-gray-800 group">
                     <Image src={"/logo.svg"} alt={"main icon"} width={32} height={32} priority />
                     <div className="flex flex-col">
-                        <span>Rex Service</span>
+                        <span className="group-hover:underline">Rex Service</span>
                         <p className="text-xs text-gray-500 font-medium">Fast and reliable service.</p>
                     </div>
                 </Link>
