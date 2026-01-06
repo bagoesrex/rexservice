66e0056573a0a0ea1e875a2ad71bb252284db3e0
bagoesrex <bagoesrex@gmail.com>
2026-01-06 17:52:00 +0700

    feat(home): add why choose section and feature card

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "./_components/hero/hero-section";
 import ServicesSection from "./_components/services/services-section";
 import StatsSection from "./_components/stats/stats-section";
+import WhyChooseSection from "./_components/whychoose/why-choose-section";
 
 export default function Home() {
   return (
@@ -10,6 +11,8 @@ export default function Home() {
       <ServicesSection />
 
       <StatsSection />
+
+      <WhyChooseSection />
 
       {/* <HowItWorksSection /> */}
     </main>
